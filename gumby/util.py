ea2455f6d3a2eb2eac49f47ff607b8145911de4e
Martijn de Vos <mdmartijndevos@gmail.com>
2020-04-14 22:03:03 +0200

    Changed coroutine functionality

--- gumby/util.py
+++ gumby/util.py
@@ -1,5 +1,5 @@
 import os
-from asyncio import coroutine, ensure_future, iscoroutinefunction, sleep
+from asyncio import coroutine, ensure_future, iscoroutinefunction, sleep, Future, iscoroutine
 
 
 def generate_keypair_trustchain():
@@ -27,23 +27,32 @@ def save_pub_key_trustchain(keypair, pubkeyfilename):
         keyfile.write(keypair.key.pk)
 
 
+def maybe_coroutine(func, *args, **kwargs):
+    value = func(*args, **kwargs)
+    if iscoroutine(value) or isinstance(value, Future):
+        return value
+
+    async def coro():
+        return value
+    return coro()
+
+
 async def interval_runner(delay, interval, task, *args):
     await sleep(delay)
     while True:
-        await task(*args)
+        await maybe_coroutine(task, *args)
         await sleep(interval)
 
 
 async def delay_runner(delay, task, *args):
     await sleep(delay)
-    await task(*args)
+    await maybe_coroutine(task, *args)
 
 
 def run_task(task, *args, delay=0, interval=0):
     if not iscoroutinefunction(task) and not callable(task):
         raise ValueError('run_task takes a (coroutine)function as a parameter')
 
-    task = task if iscoroutinefunction(task) else coroutine(task)
     if interval:
         # The default delay for looping calls is the same as the interval
         delay = interval if delay is None else delay
@@ -51,5 +60,5 @@ def run_task(task, *args, delay=0, interval=0):
     elif delay:
         task = ensure_future(delay_runner(delay, task, *args))
     else:
-        task = ensure_future(task(*args))
+        task = ensure_future(maybe_coroutine(task, *args))
     return task
